36d9c5f1803c94c590cc9a9ed623839799f7ab8a
raz-mon <razmon@post.bgu.ac.il>
2022-05-12 15:43:29 +0300

    After meeting 12.5.22

--- Project/LiS.jl
+++ Project/LiS.jl
@@ -5,27 +5,45 @@ using SparseArrays
 using LinearAlgebra
 close("all");
 
+function solveConstHelm_temp(hop,n,b,m::ComplexF64)
+    # h = [-1 2 -1]/(h^2) - [0 m 0];
+    pad = Int(n/2); #n ; #div(n,2);
+    bext = zeros(ComplexF64,n+2*pad);
+    bext[(pad+1):(pad+n)].= b;
+    hvec = zeros(ComplexF64, n+2*pad);
+    hvec[1] = hop[2];
+    hvec[2] = hop[3];
+    hvec[n] = hop[1];
+    hath = fft(hvec);
+    hatb = fft(bext);
+    hatu = hatb./hath;
+    uext = ifft(hatu);
+    u = uext[(pad+1):(pad+n)];
+    return u;
+    end
+
 function solveConstHelm(hop,n,b,m::ComplexF64)
 # h = [-1 2 -1]/(h^2) - [0 m 0];
-pad = 0; #n ; #div(n,2);
+pad = Int(n/2); #n ; #div(n,2);
 bext = zeros(ComplexF64,n+2*pad);
-bext[(pad+1):(pad+n)].= b;
+bext[1:n] .= b;
+# bext[(pad+1):(pad+n)].= b;
 hvec = zeros(ComplexF64, n+2*pad);
 hvec[1] = hop[2];
 hvec[2] = hop[3];
-hvec[n] = hop[1];
+hvec[n+2*pad] = hop[1];
 hath = fft(hvec);
 hatb = fft(bext);
 hatu = hatb./hath;
 uext = ifft(hatu);
-u = uext[(pad+1):(pad+n)];
+#u = uext[(pad+1):(pad+n)];
+u = uext[1:n];
 return u;
 end
 
 function solveConstHelmNLA(hop,n,b,m::ComplexF64)
 # h = [-1 2 -1]/(h^2) - [0 m 0];
-# pad = trunc(Int, n/2); #n ; #div(n,2);
-pad = n # trunc(Int, n/16);
+pad = Int(2*n) # Int(n/2) # trunc(Int, n/16);
 bext = zeros(ComplexF64,n+2*pad);
 bext[(pad+1):(pad+n)].= b;
 
@@ -33,27 +51,27 @@ n_w_pad = n + 2*pad;            # n with padding.
 
 gamma = zeros(n_w_pad);
 
-gamma[1]   = -sqrt(real(m))*(1.0/h^2);             # Sommerfeld radiation condition.
-gamma[end] = -sqrt(real(m))*(1.0/h^2);
+gamma[1]   = -10 * sqrt(real(m))*(1.0/h);             # Sommerfeld radiation condition. Soft ends.
+gamma[end] = -10 * sqrt(real(m))*(1.0/h);
 
 # H = spdiagm(-1=>hop[1]*ones(n-1), 0=>hop[2]*ones(n), 1=>hop[3]*ones(n-1));
 H = spdiagm(-1=>hop[1]*ones(n_w_pad-1), 0=>hop[2]*ones(n_w_pad), 1=>hop[3]*ones(n_w_pad-1));
 
 H = H + spdiagm(0=>1im*gamma);
 
-H[1,end] = hop[1];
-H[end,1] = hop[3];
+# H[1,end] = hop[1];
+# H[end,1] = hop[3];
 # u = H\b;
 u = H\bext;
 u_internal = u[(pad+1):(pad+n)];
 return u_internal;
 end
 
-n = 400;
+n = 200;
 # pad = 20;
 # n = n+2*pad
-h = 4.0/n;
-m = (0.015/(h^2))*(1.0 + 1im*0.50)          # m = k^2. In this case it is constant through space (x).
+h = 2.0/n;
+m = (0.1/(h^2))*(1.0 + 1im*0.01)          # m = k^2. In this case it is constant through space (x).
 
 # m is more or less k^2
 hop = [-1 2 -1]/(h^2) - [0 m 0];
@@ -68,14 +86,16 @@ u2 = solveConstHelmNLA(hop,n,b,m::ComplexF64)
 figure()
 plot(real(u2))
 # savefig("no_padding_w_gamma_constNLA.png")
-show()
-error()
+
+
 
 # error()
 u1 = solveConstHelm(hop,n,b,m);
 
-figure()
+# figure()
 plot(real(u1))
+show()
+error()
 
 figure()
 plot(abs.(u1-u2))
@@ -89,6 +109,8 @@ show()
 # u = ifft(hatu);
 # println(typeof(u))
 # plot(real(u))
+
+
 
 
 
