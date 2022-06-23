048e3393ba8e2bbc102fd2bb4c2e095535879bbc
almogze <almogze7@gmail.com>
2022-06-23 13:08:55 +0300

    lap2D

--- Lap2D/lap2D.jl
+++ Lap2D/lap2D.jl
@@ -18,28 +18,30 @@ function fft_conv(kernel, n, b, m::ComplexF64)
     return u;
 end
 
-In = (n::Int64)->(return spdiagm(0=>ones(n)));
+In = (n::Int64)->(return spdiagm(0=>ones(ComplexF64, n)));
 
 function matrix_conv(n, h, b, m)
     Lap1D = (h::Float64,n::Int64) -> 
-        (A = spdiagm(0=>(2/h^2)*ones(n),1=>(-1/h^2)*ones(n-1),-1=>(-1/h^2)*ones(n-1)) - Sommerfeld;
+        (A = spdiagm(0=>(2/h^2)*ones(ComplexF64, n),1=>(-1/h^2)*ones(ComplexF64, n-1),-1=>(-1/h^2)*ones(ComplexF64, n-1)); #- Sommerfeld;
         # A[1,end] = -1/h^2;            # Periodic BC.
         # A[end,1] = -1/h^2;
         A[1,1]=1/h^2;                   # Neuman BC. See NumericalPDEs to understand why.
+        A[1,1] -= 1im * sqrt(real(m)) * (1.0/h);
         A[n,n]=1/h^2;
+        A[n,n] -= 1im * sqrt(real(m)) * (1.0/h);
         return A;
         );
     
-    fact = 10 * sqrt(real(m)) * (1.0/h);
-    Sommerfeld = spdiagm(0=>zeros(n))
-    Sommerfeld[1, :] .= fact
-    Sommerfeld[:, 1] .= fact
-    Sommerfeld[end, :] .= fact
-    Sommerfeld[:, end] .= fact
-    Sommerfeld = 1im .* Sommerfeld
-
-    Lap2D = kron(In(n), Lap1D(h,n)) + kron(Lap1D(h,n), In(n)) - m .* spdiagm(0=>ones(n*n)); # - Sommerfeld;
-    print(Lap2D[1, 1])
+    # fact = 1 * sqrt(real(m)) * (1.0/h);
+    # Sommerfeld = zeros(n, n)
+    # Sommerfeld[1, :] .= fact
+    # Sommerfeld[:, 1] .= fact
+    # Sommerfeld[end, :] .= fact
+    # Sommerfeld[:, end] .= fact
+    # Sommerfeld = 1im .* Sommerfeld
+    # Sommerfeld = spdiagm(0=>Sommerfeld[:])
+    
+    Lap2D = kron(In(n), Lap1D(h,n)) + kron(Lap1D(h,n), In(n)) - m .* spdiagm(0=>ones(ComplexF64, n*n)); #- Sommerfeld;
     b = reshape(b, (n*n, 1))
     return reshape((Lap2D\b),(n,n))
 end 
@@ -61,8 +63,8 @@ function matrix_conv_without(n, h, b, m)
 end 
 
 n = 200;
-# pad = 20;
-# n = n+2*pad
+pad = n;
+# n = n+pad;
 h = 2.0/n;
 m = (0.1/(h^2))*(1.0 + 1im*0.00)          # m = k^2. In this case it is constant through space (x).
 
@@ -71,22 +73,15 @@ kernel += [[0 -1 0];[-1 4 -1];[0 -1 0]] / h^2 - m .* [[0 0 0];[0 1 0];[0 0 0]];
 # m is more or less k^2
 b = zeros(ComplexF64, n, n);
 b[div(n,2), div(n,2)] = 1.0;
+b_pad  = zeros(n+pad,n+pad)
+b_pad[1:n,1:n] .= b
 
-# temp = fft_conv(kernel, n, b, m);
-# heatmap(real.(temp))
+temp = fft_conv(kernel, n+pad, b_pad, m);
+heatmap(real.(temp))
 
 # mat = matrix_conv_without(n, h, b, m);
 mat = matrix_conv(n, h, b, m);
 heatmap(real.(mat))
-heatmap(imag.(mat))
-heatmap(abs.(mat))
+# heatmap(imag.(mat))
+# heatmap(abs.(mat))
 
-
-
-fact = 10 * sqrt(real(m)) * (1.0/h);
-Sommerfeld = spdiagm(0=>zeros(n))
-Sommerfeld[1, :] .= fact
-Sommerfeld[:, 1] .= fact
-Sommerfeld[end, :] .= fact
-Sommerfeld[:, end] .= fact
-Sommerfeld = 1im .* Sommerfeld
