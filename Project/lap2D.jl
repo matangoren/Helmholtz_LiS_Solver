a2f021a9dc0e6e745868fdb4bc2f5ecffec2fbdf
raz-mon <razmon@post.bgu.ac.il>
2022-08-04 11:25:10 +0300

    ad

--- Project/lap2D.jl
+++ Project/lap2D.jl
@@ -177,28 +177,6 @@ function dual_grid_ratio(p, n)
     return ratios;
 end
 
-<<<<<<< HEAD
-n, h, m_base, b, pad_green = init_params()
-q = rand(ComplexF64, n, n) # + 1im * rand(ComplexF64, n, n)      # Random initializaton.
-# q = q = zeros(ComplexF64, n, n);                                  # Point source at [n/4, n/4].
-# q[div(n,4), div(n,4)] = 1.0;
-ratios = zeros(ComplexF64, n, n) .+ 0.85             # Make sure this is broadcasted.
-ratios[Int(n/4)+1: Int(3n/4), Int(n/4)+1:Int(3n/4)] = ones(Int(n/2), Int(n/2))
-
-max_iter, restrt = 10, 10
-# m_base = (0.1/(h^2))*(1.0 + 1im*0.05)
-m_grid = m_base * ratios
-min_m, max_m = get_value(m_grid, findmin), get_value(m_grid, findmax);
-delta = (real(max_m) - real(min_m)) / (max_iter * restrt) + abs(imag(max_m) - imag(min_m))im / (max_iter * restrt);
-m_0_reals = collect((i for i in real(min_m):real(delta):real(max_m)))
-m_0_ims = collect((i for i in imag(min_m):imag(delta):imag(max_m)))
-m_0s = zeros(ComplexF64, size(m_0_reals)[1])
-for i in 1:size(m_0s)[1]
-    m_0s[i] = m_0_reals[i] + m_0_ims[i]im
-end
-
-x = fgmres_sequence(q, ratios, m_0s, n, h, m_base, b, pad_green, 15, 15)
-=======
 function const_grid_ratio(n) 
     return  ones(ComplexF64, n, n);
 end
@@ -231,21 +209,15 @@ dual_ratio = dual_grid_ratio(0.85, n)
 m_0s_linear = linear_m(m_base, dual_ratio, max_iter, restrt)
 m_0s_avg = avg_m(m_base, dual_ratio, max_iter, restrt)
 
->>>>>>> almog
 # The output values of fgmres are: 
 #   1. strage long matrix (40K x num of iter).
 #   2. flag (-1 for maxIter reached without converging and -9 for right hand side was zero).
 #   3. Min value.
 #   4. Number of iterations.
 #   5. The history of the gmres sequensce. 
-<<<<<<< HEAD
-# size(x[5])
-# t1 = x[3]
-=======
 x = fgmres_sequence(q, dual_ratio, m_0s_linear, max_iter, restrt)
 size(x[5])
 t1 = x[3]
->>>>>>> almog
 
 y = fgmres_sequence(q, dual_ratio, m_0s_avg, max_iter, restrt)
 size(y[5])
